46bff758454826a1692d12b358ee955b143cb8e4
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-10-19 18:04:50 -0300

    final clase 7 Express con Router

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const app = express();
+
+app.use(router);
+
+router.get("/message", function (req, res) {
+  res.send("Lista de mensajes");
+});
+
+router.post("/message", function (req, res) {
+  res.send("Mensaje añadido");
+});
+
+// app.use("/", function (req, res) {
+//   res.send("Hola");
+// });
+
+app.listen(3000);
+console.log("La app esta en http://localhost:3000");
