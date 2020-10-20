04b45e39e98610f743108023d7111bb70e5abec6
Cesar Gonzalez Groh <clgonzalezgroh@hotmail.com>
2020-10-19 21:26:49 -0300

    fin clase 10

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(router);
 
 router.get("/message", function (req, res) {
+  console.log(req.headers);
+  res.header({
+    "custom-header": "Nuestro valor personalizado",
+  });
   res.send("Lista de mensajes");
 });
 
 router.post("/message", function (req, res) {
-  res.send("Mensaje añadido");
+  console.log(req.body);
+  console.log(req.query);
+  res.status(201).send({ Error: "", body: "Creado correctamente" });
 });
 
 // app.use("/", function (req, res) {
